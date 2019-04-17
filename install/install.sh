74816d64b52a2f06277a45858d2624d9d76ca0a7
PJFonseca <pfonseca7@gmail.com>
2019-04-17 20:21:26 +0100

    Some fixes on the install

--- install/install.sh
+++ install/install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 #Repos and Basics
+sudo dnf -y update
+sudo dnf upgrade --best --allowerasing --refresh -y
+hostnamectl set-hostname --static "fedorabox"
 sudo dnf install fedora-workstation-repositories
 sudo dnf install -y steam --enablerepo=rpmfusion-nonfree-steam
 sudo dnf install -y wget
+
+#VSCode stuff
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
 
 # YouTube Downloaded -> https://ytdl-org.github.io/youtube-dl/index.html
 sudo dnf install -y youtube-dl
@@ -12,16 +20,38 @@ sudo dnf install -y youtube-dl
 sudo dnf install -y numlockx
 
 ##Install Software
-sudo dnf install -y nano 
-sudo dnf install -y vlc
-sudo dnf install -y visual-studio-code
-sudo dnf install -y dropbox
-sudo dnf install -y firefox
-sudo dnf install -y filezilla
-sudo dnf install -y audacity
-sudo dnf install -y keepassxc
-sudo dnf install -y telegram-desktop
-sudo dnf install -y qbittorrent
+
+sudo dnf install \
+-y \
+nano \
+arc-theme \
+pop-icon-theme \
+breeze-cursor-theme \
+vlc \
+code \
+dropbox \
+firefox \
+filezilla \
+audacity \
+keepassxc \
+telegram-desktop \
+qbittorrent \
+exfat-utils \
+ffmpeg \
+git \
+nautilus-extensions \
+nautilus-image-converter \
+nautilus-search-tool 
+
+
+# Remove un-needed stuff
+sudo dnf remove \
+-y \
+gnome-shell-extension-background-logo \
+totem \
+chromium \
+flowblade
+
 
 #TeamViewer
 wget -P /tmp/ https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm && sudo dnf install -y /tmp/teamviewer.x86_64.rpm
@@ -40,6 +70,21 @@ wget -P /tmp/ -O gnome-shell-extension-installer "https://github.com/brunelli/gn
 #Install
 gnome-shell-extension-installer 1160 # Dash to Panel by jderose9 
 gnome-shell-extension-installer 1112 # Screenshot Tool by oal
+
+#Install flat-remix-gtk theme
+cd /tmp && rm -rf flat-remix-gtk && git clone https://github.com/daniruiz/flat-remix-gtk && mkdir -p ~/.themes && cp -r flat-remix-gtk/Flat-Remix-GTK* ~/.themes/
+
+#Interface
+gsettings set org.gnome.desktop.interface gtk-theme 'Flat-Remix-GTK-Dark'
+gsettings set org.gnome.desktop.interface cursor-theme 'Breeze_Snow'
+gsettings set org.gnome.desktop.interface icon-theme 'Flat-Remix-Blue-Dark'
+
+#Nautilus
+gsettings set org.gnome.nautilus.icon-view default-zoom-level 'standard'
+gsettings set org.gnome.nautilus.preferences executable-text-activation 'ask'
+gsettings set org.gtk.Settings.FileChooser sort-directories-first true
+gsettings set org.gnome.nautilus.list-view use-tree-view true
+
 
 #Codecs
 sudo dnf install gstreamer1-{plugin-crystalhd,ffmpeg,plugins-{good,ugly,bad{,-free,-nonfree,-freeworld,-extras}{,-extras}}} libmpg123 lame-libs --setopt=strict=0 -y
