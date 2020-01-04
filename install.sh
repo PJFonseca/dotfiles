609454c0e0ace1f726127bce0de3916f5d7af627
PJFonseca <pfonseca7@gmail.com>
2020-01-04 07:46:15 +0000

    VSCode Install

--- install.sh
+++ install.sh
@@ -74,6 +74,18 @@ cd /tmp/ && wget -P /tmp/ --trust-server-names https://go.skype.com/skypeforlinu
 cd /tmp/ && wget -P /tmp/ --trust-server-names https://telegram.org/dl/desktop/linux | sudo tar xJ -C /opt/
 sudo ln -s /opt/Telegram/Telegram /usr/local/bin/telegram-desktop
 
+#VSCode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+cat <<EOF | sudo tee /etc/yum.repos.d/vscode.repo
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+sudo dnf install code
+
 #Postman
 wget -P /tmp/ https://dl.pstmn.io/download/latest/linux64 -O postman-linux-x64.tar.gz
 sudo tar xvzf /tmp/postman-linux-x64.tar.gz -C /opt
