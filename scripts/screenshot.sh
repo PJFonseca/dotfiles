cdd53df11f9f25b09fbf45480aa3fd22d76dc519
PJFonseca <pfonseca7@gmail.com>
2021-12-27 12:28:53 +0000

    New bindings

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 ##ssarea script
 DATE=$(date +%Y-%m-%d-%H:%M:%S)
-gnome-screenshot -a -f $HOME/$IMAGES_FOLDER/Screenshot-$DATE.png
-gnome-screenshot -a -c $HOME/$IMAGES_FOLDER/Screenshot-$DATE.png
+gnome-screenshot -a -f -c $HOME/$IMAGES_FOLDER/Screenshot-$DATE.png
