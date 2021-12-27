db6781660747efbc5a653b54138141ad1b95b0da
PJFonseca <pfonseca7@gmail.com>
2021-12-27 12:15:50 +0000

    New script for screenshots

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -2,3 +2,4 @@
 ##ssarea script
 DATE=$(date +%Y-%m-%d-%H:%M:%S)
 gnome-screenshot -a -f $HOME/$IMAGES_FOLDER/Screenshot-$DATE.png
+gnome-screenshot -a -f -c $HOME/$IMAGES_FOLDER/Screenshot-$DATE.png
