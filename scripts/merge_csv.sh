da31e6b494fe89cbb4c399a552af0fe107cae3f7
PJFonseca <pedro@fonseca.com.pt>
2024-04-25 13:59:48 +0100

    Fixes

--- scripts/merge_csv.sh
+++ scripts/merge_csv.sh
@@ -12,10 +12,19 @@ if [ -f "CSV/PageOrder.csv" ]; then
     echo "CSV/PageOrder.csv deleted."
 fi
 
-# Unzip all .zip files into CSV directory
+# Temporary directory to unzip files
+temp_dir="temp_unzip"
+
+# Unzip all .zip files into temporary directory
 for zip_file in *.zip; do
-    unzip "$zip_file" -o -j -d CSV/
+    unzip -o "$zip_file" -d "$temp_dir/"
 done
+
+# Move all .csv files from temporary directory to main CSV directory
+find "$temp_dir" -name "*.csv" -exec mv {} CSV/ \;
+
+# Remove temporary directories
+rm -r "$temp_dir"
 
 # Merge CSV files excluding headers except the first file
 cat CSV/*.csv | awk -F',' 'NR == 1 || (FNR > 1 && $1 != "Respondent ID" && $10 != "Response")' > merged.csv
